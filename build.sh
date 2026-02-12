9445e17ec08d6b2af2d169a844f5a6efd26ee78f
Boris Tobotras <tobotras@gmail.com>
2026-02-12 12:53:57 +0100

    Universal binary

--- build.sh
+++ build.sh
@@ -10,7 +10,11 @@ cd "$(dirname "$0")"
 rm -rf "$APP_BUNDLE"
 mkdir -p "$APP_BUNDLE/Contents/MacOS"
 
-swiftc -O -o "$APP_BUNDLE/Contents/MacOS/$APP_NAME" Sources/main.swift -framework Cocoa
+# Build universal binary (Intel + Apple Silicon)
+swiftc -O -target arm64-apple-macosx12.0 -o "/tmp/${APP_NAME}_arm64" Sources/main.swift -framework Cocoa -framework WebKit
+swiftc -O -target x86_64-apple-macosx12.0 -o "/tmp/${APP_NAME}_x86" Sources/main.swift -framework Cocoa -framework WebKit
+lipo -create "/tmp/${APP_NAME}_arm64" "/tmp/${APP_NAME}_x86" -output "$APP_BUNDLE/Contents/MacOS/$APP_NAME"
+rm "/tmp/${APP_NAME}_arm64" "/tmp/${APP_NAME}_x86"
 cp Info.plist "$APP_BUNDLE/Contents/Info.plist"
 
 echo "✔ Built $APP_BUNDLE"
